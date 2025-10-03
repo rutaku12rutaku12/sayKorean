e18fff4b34a5eac97e309aa86fb2f4e1964f2d94
yj08-80 <{email}>
2025-10-03 17:45:23 +0900

    3_정유진_StudyController,StudyMapper,StudyService_장르목록조회

--- src/main/java/web/controller/StudyController.java
+++ src/main/java/web/controller/StudyController.java
@@ -1,13 +1,29 @@
 package web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import web.model.dto.GenreDto;
+import web.model.mapper.StudyMapper;
+import web.service.StudyService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/saykorean/study")
 @RequiredArgsConstructor
 public class StudyController {
+
+    private final StudyService studyService;
+
+
+    // 사용자단 : 장르 목록 조회
+    @GetMapping("/getGenre")
+    public ResponseEntity< List< GenreDto > > getGenre() {
+        return ResponseEntity.ok( studyService.getGenre() );
+    }
 
 
 }
