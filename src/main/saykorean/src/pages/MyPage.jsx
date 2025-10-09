1836690d55b71ec51ed3bd4c15916c8c397c1434
rutaku12rutaku12 <chookyuho@gmail.com>
2025-10-09 23:29:04 +0900

    16_최규호_MyPage.jsx 작성중

--- src/main/saykorean/src/pages/MyPage.jsx
+++ src/main/saykorean/src/pages/MyPage.jsx
@@ -1,7 +1,12 @@
 export default function MyPage( props ){
     console.log("MyPage.jsx open")
     return(<>
-    Email · 
+    닉네임 · 가입일자       설정버튼 <br/>
+    
+    장르번호<br/>
+
+    출석일자<br/>
+
     </>)
     
 }
---
+++ src/main/saykorean/src/pages/SignUpPage.jsx
@@ -0,0 +1,10 @@
+export default function SignUpPage (props){
+    return(<>
+    이름: 
+    이메일:
+    비밀번호:
+    닉네임:
+    연락처:
+    장르:
+    </>)
+}
