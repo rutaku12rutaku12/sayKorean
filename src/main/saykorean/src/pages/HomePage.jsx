3dd82a769993c77837227489b17ed90d4eb39160
rutaku12rutaku12 <chookyuho@gmail.com>
2025-10-11 23:15:27 +0900

    17_최규호_메인페이지 이미지 추가

--- src/main/saykorean/src/pages/HomePage.jsx
+++ src/main/saykorean/src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
 export default function HomePage ( props ){
 
     return (<>
-        <h3> 홈페이지 </h3>
+    <h3>재밌는 한국어</h3>
+            <div>
+                <img className="mainImg" src="/img/mainimage.svg"/>
+                
+                회원가입 로그인
+            </div>
+            
     </>)
 }
