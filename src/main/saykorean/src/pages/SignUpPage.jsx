6a1264e85d8e008029c9d4e08d53e75d29fe8d14
rutaku12rutaku12 <chookyuho@gmail.com>
2025-10-13 17:19:09 +0900

    21_최규호_음성파일 추가, 회원가입 작성중

--- src/main/saykorean/src/pages/SignUpPage.jsx
+++ src/main/saykorean/src/pages/SignUpPage.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom";
@@ -8,20 +9,48 @@ export default function SignUpPage (props){
     // disptach , navigate 함수 가져오기
     const dispatch = useDispatch();
     const navigate = useNavigate();
-
+    // 인풋 상태 관리 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [nickName, setNickname] = useState("");
     const [phone, setPhone] = useState("");
-    const [genreNo, setGenreNo] 
+                    // const [genreNo, setGenreNo] = useState(""); 기본값1
+    
+    // 중복 여부 상태 관리 
+    const [emailCheck, setEamilCheck] = useState(false);
+    
 
-    return(<>
-    이름: 
-    이메일:
-    비밀번호:
-    닉네임:
-    연락처:
-    장르:
+    
+    // 중복검사 
+    const CheckEmail = async () =>{
+        try{
+            // CORS 옵션 허용
+            const option = { withCredentials : true }
+            const response = await axios.get(`http://localhost:8080/saykorean/checkemail?email=${email}`,option)
+            console.log(response);
+            const data = response.data;
+            console.log("중복여부", data)
+
+        }catch (e){
+            console.log("중복 : " ,e)
+        }
+    }
+
+    return(<> <h3>회원가입</h3><br/>
+        <div>
+            이름 (name) <br/>
+            <input type="name" placeholder="이름을 입력해주세요." value={name} onChange={(e)=> setName(e.target.value)} /> <br/>
+            이메일 (email) <br/>
+            <input type="email" placeholder="이메일을 입력해주세요." value={email} onChange={(e)=> setEmail(e.target.value)} /> <button onClick={CheckEmail}> 중복확인</button> <br/>
+            비밀번호 (password) <br/>
+            <input type="password" placeholder="비밀번호를 입력해주세요." value={password} onChange={(e)=> setPassword(e.target.value)} /> <br/>
+            닉네임 (nickName) <br/>
+            <input type="nickName" placeholder="닉네임을 입력해주세요." value={nickName} onChange={(e)=> setNickname(e.target.value)} /> <br/>
+            연락처 (phone) <br/>
+            <input type="phone" placeholder="연락처를 입력해주세요." value={phone} onChange={(e)=> setPhone(e.target.value)} /> <button> 중복확인</button> <br/>
+            <br/>
+            <button>회원가입 (SignUp) </button>
+        </div>
     </>)
 }
