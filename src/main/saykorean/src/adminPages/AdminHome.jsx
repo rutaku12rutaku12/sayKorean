a16a30ba45312f62b483ee114b55c8a02e2c5267
kafella92 <choidj10@gmail.com>
2025-10-14 18:14:27 +0900

    19_최동진_리액트기강잡기

--- src/main/saykorean/src/adminPages/AdminHome.jsx
+++ src/main/saykorean/src/adminPages/AdminHome.jsx
@@ -1,5 +1,9 @@
 export default function AdminHome(props) {
     return (<>
         <h3> 관리자 홈 페이지 </h3>
+        여기에 토돌이 호순이 그림 넣기
+
+        교육 등록하기
+        교육 목록으로 이동
     </>)
 }
--- src/main/saykorean/src/adminPages/AdminStudyCreate.jsx
+++ src/main/saykorean/src/adminPages/AdminStudyCreate.jsx
@@ -1,8 +1,22 @@
 export default function AdminStudyCreate(props) {
 
+    // [1] 장르 Axios
+
+    // [2] 주제 Axios
+
+    // [3-1] 예문 레코드 Axios
+
+    // [3-2] 예문 그림파일 Axios
+
+    // [4-1] 음성 레코드 Axios
+
+    // [4-2] 음성 파일 Axios
+
+    // [*] 교육 생성 로직
+
+    // [*] 교육 
 
 
-    
     return (<>
         <h3> 관리자 교육 생성 </h3>
     </>)
