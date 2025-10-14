794f51091c9fab3b396dc0278a3d277a54148358
kafella92 <choidj10@gmail.com>
2025-10-14 17:57:27 +0900

    18_최동진_리액트시작

--- src/main/saykorean/src/adminPages/AdminStudyCreate.jsx
+++ src/main/saykorean/src/adminPages/AdminStudyCreate.jsx
@@ -1,4 +1,8 @@
 export default function AdminStudyCreate(props) {
+
+
+
+    
     return (<>
         <h3> 관리자 교육 생성 </h3>
     </>)
--- src/main/saykorean/src/adminPages/AdminStudyList.jsx
+++ src/main/saykorean/src/adminPages/AdminStudyList.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react"
+
 export default function AdminStudyList(props) {
+
+    // 교육 전체 출력 로직
+    useEffect
+
     return (<>
         <h3> 관리자 교육 목록 </h3>
     </>)
