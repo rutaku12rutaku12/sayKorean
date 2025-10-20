4c37d407b566dae29edd6358aeaec592d3165fbb
yj08-80 <{email}>
2025-10-20 16:38:13 +0900

    44_정유진_TestMapper_랭킹테이블저장Duplicate제거

--- src/main/java/web/model/mapper/TestMapper.java
+++ src/main/java/web/model/mapper/TestMapper.java
@@ -29,12 +29,12 @@ public interface TestMapper { // mapper start
     // 전체 점수 출력 - 로직 : 랭킹테이블에 저장 -> 점수 집계
 
     // 랭킹테이블 저장
-    @Insert("INSERT INTO ranking (testRound, selectedExamNo, userAnswer, isCorrect, testItemNo, userNo) VALUES (#{testRound}, #{selectedExamNo}, #{userAnswer}, #{isCorrect}, #{testItemNo}, #{userNo})ON DUPLICATE KEY UPDATE selectedExamNo = VALUES(selectedExamNo), userAnswer = VALUES(userAnswer), isCorrect = VALUES(isCorrect), resultDate = CURRENT_TIMESTAMP")
+    @Insert("INSERT INTO ranking (testRound, selectedExamNo, userAnswer, isCorrect, testItemNo, userNo, resultDate) VALUES (#{testRound}, #{selectedExamNo}, #{userAnswer}, #{isCorrect}, #{testItemNo}, #{userNo}, NOW())")
     int upsertRanking( RankingDto dto );
 
     // 점수 집계: 해당 시험(testNo) & 회차(testRound)에서
-// - score = 정답 개수
-// - total = 전체 문항 수
+    // - score = 정답 개수
+    // - total = 전체 문항 수
 
     @Select("SELECT SUM(CASE WHEN r.isCorrect = 1 THEN 1 ELSE 0 END) AS score, COUNT(*) AS total FROM ranking r JOIN testItem ti ON ti.testItemNo = r.testItemNo WHERE r.userNo = #{userNo} AND r.testRound = #{testRound} AND ti.testNo = #{testNo}")
     RankingDto getScore( int userNo , int testNo , int testRound );
