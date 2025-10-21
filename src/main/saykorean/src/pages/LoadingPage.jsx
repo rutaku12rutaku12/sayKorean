1516dbb7f536c41443ebb0066220a634f94bbb65
rutaku12rutaku12 <chookyuho@gmail.com>
2025-10-21 18:07:20 +0900

    55_최규호_로딩 수정사진 추가/변경중

--- src/main/saykorean/src/pages/LoadingPage.jsx
+++ src/main/saykorean/src/pages/LoadingPage.jsx
@@ -1,16 +1,24 @@
 import "../styles/LoadingPage.css";
 export default function LoadingPage( props ){
 
-    return (<>
+    return (
     <div id="loading-frame">
-      <img width="410px" src="/img/loading_img/1_loading_img.png" />
-        <div id="loading-footer">
-            <h3>토돌이 당근 수집하는중 </h3>
+      <div className="image-container">
+        <h1>"숭레문"</h1>
+        <img width="410px" src="/img/loading_img/1_loading_img.png" />
+        <div className="text-on-image">
+          <h3>남대문이라고도 불립니다.</h3>
         </div>
+      </div>
+
+      <div id="loading-footer">
+        <h3>토돌이 당근 수집하는중</h3>
+      </div>
     </div>
-    
-  </>)
+  );
 
 
     
 }
+
+
