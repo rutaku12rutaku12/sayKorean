38bc939d5600980da495feb55858359843a087a2
rutaku12rutaku12 <chookyuho@gmail.com>
2025-10-22 01:38:46 +0900

    56_최규호_비로그인시 404페이지 이동 로직 Test.jsx에서 TestList.jsx 로 이동, App.jsx HomePage 라우터 경로 수정, 로딩 페이지 사진5개추가

--- src/main/saykorean/src/pages/TestList.jsx
+++ src/main/saykorean/src/pages/TestList.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { logIn } from "../store/userSlice";
 
 export default function TestList( props ){
 
@@ -26,6 +28,34 @@ export default function TestList( props ){
             }
         })();
      }, []);
+
+    // store 저장된 상태 가져오기
+    const {isAuthenticated, userInfo } = useSelector((state)=>state.user);
+    // dispatch , navigate 함수가져오기
+        const dispatch = useDispatch();
+    // 최초 1번 렌더링
+        useEffect( () => { info(); } , [] )
+    // 내 정보 조회 함수
+    const info = async () => {
+        try{console.log("info.exe")
+            const option = { withCredentials : true }
+            const response = await axios.get("http://localhost:8080/saykorean/info",option)
+            const data = response.data
+            console.log(data);
+            // 로그인된 데이터 정보를 userInfo에 담기
+            dispatch(logIn(data));
+        }catch(e){console.log(e)}
+    }
+    // 비로그인시 error 페이지로 이동
+    useEffect(() => {
+        if (!isAuthenticated) {
+        navigate("/error"); // 로그인 안 되어 있으면 바로 이동
+        }
+    }, [isAuthenticated, navigate]);
+
+    // 이동 전에 화면 깜빡임 방지
+    if (!isAuthenticated) return null;
+
 
     return (<>
         <div id="TestList">
