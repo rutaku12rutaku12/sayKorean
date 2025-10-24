a827fd23c03ee014612517cb542e0d28c986da30
kafella92 <choidj10@gmail.com>
2025-10-24 18:23:15 +0900

    53_최동진_Language4번임포트주석처리하니까돌아감

--- src/main/saykorean/src/pages/Language.jsx
+++ src/main/saykorean/src/pages/Language.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import i18n, { setAppLanguage } from "../i18n";
+// import i18n, { setAppLanguage } from "../i18n";
 axios.defaults.baseURL = "http://localhost:8080";
 
 
