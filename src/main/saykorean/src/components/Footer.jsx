a95be3aa9d15262c377fae07ce07eee0054ca4cb
MSI <kgs2072@naver.com>
2025-10-28 01:29:08 +0900

    강사. 메인페이지 (반응형)레이아웃 1. App.css 2. HomePage.css 3. Footer.jsx 4. Footer.css

--- src/main/saykorean/src/components/Footer.jsx
+++ src/main/saykorean/src/components/Footer.jsx
@@ -5,14 +5,15 @@ export default function Footer(props) {
 
     return (<>
         {/* <h3> 푸터 </h3> */}
-        <div id="footer">
+        <ul id="footer">
             <ul className="list">
-                <li> <Link to="/home"> <img src="/img/home.svg"/></Link> </li>
-                <li> <Link to="/mypage"><img src="/img/myPage.svg"/></Link> </li>
-                <li> <Link to="/beforestudy"><img src="/img/study.svg"/></Link> </li>
-                <li> <Link to="/testlist"><img src="/img/test.svg"/></Link> </li>
-                {/* <li> <Link to="/rank"><img src="/img/rank.svg"/></Link></li> */}
+                <li> <Link to="/home"> <img src="/img/home.svg"/></Link> <div>홈</div></li>
+                <li> <Link to="/mypage"><img src="/img/myPage.svg"/></Link> <div>내정보</div> </li>
+                <li> <Link to="/beforestudy"><img src="/img/study.svg"/></Link> <div>학습</div> </li>
+                <li> <Link to="/testlist"><img src="/img/test.svg"/></Link> <div>시험</div> </li>
+                <li> <Link to="/rank"><img src="/img/test.svg"/></Link> <div>순위</div> </li>
+                {/* <li> <Link to="/rank"><img src="/img/rank.svg"/></Link><div>홈</div></li> */}
             </ul>
-        </div>
+        </ul>
     </>)
 }
