e93311e4c958e9261516b1f093f4061bd774c376
kafella92 <choidj10@gmail.com>
2025-10-27 16:19:03 +0900

    59_최동진_회원관리_개별페이지제작중

--- src/main/saykorean/src/components/AdminNav.jsx
+++ src/main/saykorean/src/components/AdminNav.jsx
@@ -5,13 +5,14 @@ export default function AdminNav(props) {
     return (
         <div className="admin-nav">
             <div>
-                <Link to="/admin"> <img className="navImg" src="favicon.ico" /> </Link>
+                <Link to="/admin"> <img className="navImg" src="/img/todori.png" /> </Link>
                 <span className="navTitle"> 재밌는한국어 관리자 </span>
             </div>
             <ul className="list">
                 <li><Link to="/admin">관리자 홈</Link></li>
                 <li><Link to="/admin/study">교육 관리</Link></li>
                 <li><Link to="/admin/test">시험 관리</Link></li>
+                <li><Link to="/admin/user">회원 관리</Link> </li>
             </ul>
         </div >
     )
