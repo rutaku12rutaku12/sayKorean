14f35ccf0621325929b529011220408f76de1dc5
kafella92 <choidj10@gmail.com>
2025-10-28 14:39:14 +0900

    64_최동진_오후취합

---
+++ src/main/java/web/config/WebConfig.java
@@ -0,0 +1,22 @@
+package web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // application.properties에서 파일 업로드 경로 주입
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // URL 경로가 '/upload/**' 패턴으로 시작하는 모든 요청에 대해
+        registry.addResourceHandler("/upload/**")
+                // 실제 파일 시스템의 'uploadPath' 경로에서 파일을 찾아 제공함
+                // 'file:' 프리픽스는 파일 시스템 경로임을 명시
+                .addResourceLocations("file:" + uploadPath);
+    }
+}
